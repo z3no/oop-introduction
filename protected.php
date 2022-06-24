2b53335720c52aad6b8c8ba1608cc264d2b1e7dc
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-24 09:22:31 +0200

    finished exercise 4

---
+++ protected.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+/* EXERCISE 4
+Copy the code of exercise 2 to here and delete everything related to cola.
+TODO: Make all properties protected.
+TODO: Make all the other prints work without error without changing the beverage class.
+USE TYPEHINTING EVERYWHERE!
+*/
+
+class Beverage {
+
+    protected string $color;
+    protected float $price;
+    protected string $temperature;
+
+    /**
+     * @param string $color
+     * @param float $price
+     * @param string $temperature
+     */
+
+    public function __construct(string $color, float $price, string $temperature="cold")
+    {
+        // "set this class's properties with the same value as the incoming parameters"
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getBeverageInfo() : string {
+        return "This beverage is $this->temperature and $this->color.";
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param string $color
+     */
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+
+}
+
+class Beer extends Beverage {
+
+    protected string $name;
+    protected float $alcoholPercentage;
+
+    /**
+     * @param string $name
+     * @param float $alcoholPercentage
+     */
+    public function __construct(string $color, float $price, string $name, float $alcoholPercentage)
+    {
+        parent::__construct($color, $price);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAlcoholPercentage(): float
+    {
+        return $this->alcoholPercentage;
+    }
+
+    /**
+     * @param float $alcoholPercentage
+     */
+    public function setAlcoholPercentage(float $alcoholPercentage): void
+    {
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+
+}
+
+$duvel = new Beer("blond", 3.5, "Duvel", 8.5);
+
+echo $duvel->getAlcoholPercentage();
+echo "<br/>";
+
+$duvel->setAlcoholPercentage(8.5);
+echo $duvel->getAlcoholPercentage();
+echo "<br/>";
+
+echo $duvel->getColor();
+echo "<br/>";
+
+echo $duvel->getBeverageInfo();
